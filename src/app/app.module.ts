bd65904902a940344aded6f39a4a138bbefe9e64
Peerapol Panmuang <peerapol-srs@Peerapol-SRS.local>
2022-05-03 17:36:45 +0700

    165 add edit recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -22,6 +23,7 @@ import  {AppRoutingModule} from './app.routing.module';
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
+    RecipeEditComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
